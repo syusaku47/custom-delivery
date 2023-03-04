2900e462ed284de3dc27a402af3245be41abba82
桝田@マリエッタ <masuda.s@marietta.co.jp>
2023-03-04 14:54:55 +0900

    [Add] 配送テーブル作成

---
+++ src/database/migrations/2023_03_04_052024_create_deliveries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeliveriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->increments('id')->comment('プランID');
+            $table->integer('prefecture_id')->unsigned()->unique()->comment('都道府県ID');
+            $table->integer('delivery_days')->unsigned()->comment('配送期間');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deliveries');
+    }
+}
